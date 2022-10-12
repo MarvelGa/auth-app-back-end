8eec5bb925f66910113af7e33cb4414d0311bfbf
Halyna Tesliuk <halyna_tesliuk@epam.com>
2022-10-12 18:49:36 +0300

    added the one time saving the users in DB before getting any request from UI

--- src/main/java/com/auth/controller/UserController.java
+++ src/main/java/com/auth/controller/UserController.java
@@ -2,21 +2,22 @@ package com.auth.controller;
 
 import com.auth.dto.UserDto;
 import com.auth.service.UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/users")
-
 public class UserController {
     private final UserService userService;
 
+    public UserController(UserService userService) {
+        this.userService = userService;
+        userService.saveUsersToDb();
+    }
+
     @PostMapping
     public UserDto getUserByItsCredentials(@RequestBody UserDto request){
-         userService.saveUsersToDb();
          return userService.findByLoginAndPassword(request);
     }
 }
